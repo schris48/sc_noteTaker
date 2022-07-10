0ad7d214dc37a731678329abd015f9d5ca45dec9
Schris <tovechris48@gmail.com>
2022-07-10 18:49:19 -0400

    create-note-functionality

--- Develop/public/assets/js/notes.js
+++ Develop/public/assets/js/notes.js
@@ -17,3 +17,33 @@ const findId = (id, notesArr) => {
   return result;
 };
 
+// edit note
+const editNote = (changedNote, notesArr) => {
+  const index = notesArr.findIndex(note => note.id === changedNote.id);
+  
+  // replace changed note with existing note
+  notesArr.splice(index, 1);
+  notesArr.splice(index, 0, changedNote);
+
+  // sync changed note with db
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notes: notesArr }, null, 2)
+  )
+
+};
+
+// delete note
+const deleteNote = (note, notesArr) => {
+  // delete note by id
+  const index = notesArr.indexOf(note);
+  notesArr.splice(index, 1);
+
+  // replace db with new arr
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({  notes: notesArr }, null, 2)
+  );
+};
+
+module.exports = { newNote, findId, editNote, deleteNote };
